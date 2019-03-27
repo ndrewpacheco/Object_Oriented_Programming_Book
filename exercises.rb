67e11a913640d65804f3b7c78a97c5a49f32b4d6
Andrew Pacheco <ndrewpacheco@gmail.com>
2019-03-27 13:15:34 -0500

    first commit

---
+++ exercises.rb
@@ -0,0 +1,19 @@
+# exercises
+
+# How do we create an object in Ruby? Give an example of the creation of an object.
+
+class Example
+
+  include ModuleExample
+
+end
+
+example = Example.new
+
+# a module is a collection of behaviors that is usable in other classes via mixins
+
+
+module ModuleExample
+
+
+end
